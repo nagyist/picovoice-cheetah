122f35ee805bfd3e51e936a4d27db399afd7a6a5
Matthew Maxwell <matthew@picovoice.ai>
2024-06-26 17:25:15 -0700

    added script to interface with browserstack and update android workflow

---
+++ script/automation/browserstack.py
@@ -0,0 +1,83 @@
+import argparse
+import requests
+import os
+
+APP_URI = 'https://api-cloud.browserstack.com/app-automate/espresso/v2/app'
+TEST_URI = 'https://api-cloud.browserstack.com/app-automate/espresso/v2/test-suite'
+BUILD_URI = 'https://api-cloud.browserstack.com/app-automate/espresso/v2/build'
+
+def main(args: argparse.Namespace) -> None:
+    app_files = {
+        'file': open(args.app_path, 'rb')
+    }
+    app_response = requests.post(
+        APP_URI,
+        files=app_files,
+        auth=(args.username, args.access_key)
+    )
+    app_response_json = app_response.json()
+
+    print(app_response)
+    print(app_response_json)
+
+    if not app_response.ok:
+        exit(1)
+
+    test_files = {
+        'file': open(args.test_path, 'rb')
+    }
+    test_response = requests.post(
+        TEST_URI,
+        files=test_files,
+        auth=(args.username, args.access_key)
+    )
+    test_response_json = test_response.json()
+
+    print(test_response)
+    print(test_response_json)
+
+    if not test_response.ok:
+        exit(1)
+
+    build_headers = {
+        'Content-Type': 'application/json'
+    }
+    build_data = {
+        'app': app_response_json['app_url'],
+        'testSuite': test_response_json['test_suite_url'],
+        'project': args.project_name,
+        'devices': [
+            'Google Pixel 6 Pro-12.0'
+        ]
+    }
+    build_response = requests.post(
+        BUILD_URI,
+        headers=build_headers,
+        json=build_data,
+        auth=(args.username, args.access_key)
+    )
+    build_response_json = build_response.json()
+
+    print(build_response)
+    print(build_response_json)
+
+    if not build_response.ok:
+        exit(1)
+
+    if build_response_json['message'] != 'Success':
+        exit(1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username', required=True)
+    parser.add_argument('--access_key', required=True)
+
+    parser.add_argument('--project_name', required=True)
+    parser.add_argument('--app_path', required=True)
+    parser.add_argument('--test_path', required=True)
+    args = parser.parse_args()
+
+    os.system('ls')
+    os.system('ls cheetah-test-app/build/outputs/apk/androidTest/debug/')
+
+    # main(args)
